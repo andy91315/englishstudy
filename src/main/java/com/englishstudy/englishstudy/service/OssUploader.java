4ce5650677299617d48d6704aef2602daccdfd8b
Andi Shen <andishen@microsoft.com>
2024-12-05 22:48:36 +0800

    fix plugin and add ossUploader

---
+++ src/main/java/com/englishstudy/englishstudy/service/OssUploader.java
@@ -0,0 +1,53 @@
+package com.englishstudy.englishstudy.service;
+
+import com.aliyun.oss.ClientBuilderConfiguration;
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.auth.*;
+import com.aliyun.oss.common.comm.SignVersion;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+import java.io.File;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+public class OssUploader {
+
+    private OSS ossClient;
+    private final String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+    private final String bucketName = "examplebucket";
+    private final String region = "cn-hangzhou";
+
+    public OssUploader() throws com.aliyuncs.exceptions.ClientException {
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+        
+        // 创建OSSClient实例。
+        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
+        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);  
+        this.ossClient = OSSClientBuilder.create()
+                .endpoint(endpoint)
+                .credentialsProvider(credentialsProvider)
+                .clientConfiguration(clientBuilderConfiguration)
+                .region(region)
+                .build();
+    }
+
+    public void uploadFile(String filePath) {
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "exampledir/" + new File(filePath).getName();
+
+        try {
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new File(filePath));
+            // 上传文件。
+            ossClient.putObject(putObjectRequest);
+        } catch (OSSException | ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
