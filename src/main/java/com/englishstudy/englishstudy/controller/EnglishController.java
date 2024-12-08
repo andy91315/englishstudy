908ad770c2122cbafc1ef39311bb2d1b50e857da
Andi Shen <andishen@microsoft.com>
2024-12-08 11:19:11 +0800

    Add front end code

--- src/main/java/com/englishstudy/englishstudy/controller/EnglishController.java
+++ src/main/java/com/englishstudy/englishstudy/controller/EnglishController.java
@@ -1,13 +1,13 @@
 package com.englishstudy.englishstudy.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class EnglishController {
 
-    @RequestMapping("/test")
-    public String testRest() {
-        return "hello world";
+    @GetMapping("/index")
+    public String index() {
+        return "index"; // 返回视图名称
     }
 }
